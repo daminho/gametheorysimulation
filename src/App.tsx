9be4e62b7919338b0144650fad7ed61f619eba5b
hoangdz <dangminhhoangdz@gmail.com>
2024-05-26 23:10:38 +0900

    change config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import MainPage from './components/MainPage';
-import DoveHawk from './games/DoveHawk';
 import PrisonerDilemma from './games/PrisonerDilemma/PrisonerDilemma';
 
 function App() {
